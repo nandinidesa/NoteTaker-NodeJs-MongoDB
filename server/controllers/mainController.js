4b26b12337813434360db0f3c87305a5847b67f1
Nandini <Nandini>
2024-07-17 20:09:57 +0530

    third commit

---
+++ server/controllers/mainController.js
@@ -0,0 +1,28 @@
+/**
+ * GET /
+ * Homepage
+ */
+exports.homepage = async(req, res) => {
+    const locals={
+        title: 'NodeJs Notes',
+        description: 'Free NodeJs Notes App'
+    }
+
+    res.render('index',{
+        locals,
+        layouts: '../views/layouts-front-page'
+        });
+}
+
+/**
+ * GET /
+ * About
+ */
+exports.about = async(req, res) => {
+    const locals={
+        title: 'About NodeJs Notes',
+        description: 'Free NodeJs Notes App'
+    }
+
+    res.render('about',locals);
+}
